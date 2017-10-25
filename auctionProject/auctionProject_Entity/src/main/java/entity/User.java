054b0b056a23a7a32e4043165d5fa9e9346f3eec
simo <tibasimo@gmail.com>
2017-10-25 17:25:41 +0200

    final

--- auctionProject/auctionProject_Entity/src/main/java/entity/User.java
+++ auctionProject/auctionProject_Entity/src/main/java/entity/User.java
@@ -16,9 +16,9 @@ public class User extends ParentOU implements Serializable {
 	private int codep;
 	private String email;
 	private boolean sell_rights;
-	//private Auction ect;
+	private Auction ect;
 	
-	public User(String pseudo, String fn, String ln, int codep, String email, boolean sell_rights) {
+	public User(String pseudo, String fn, String ln, int codep, String email, boolean sell_rights, Auction ect) {
 		super();
 		this.pseudo = pseudo;
 		this.fn = fn;
@@ -26,66 +26,65 @@ public class User extends ParentOU implements Serializable {
 		this.codep = codep;
 		this.email = email;
 		this.sell_rights = sell_rights;
+		this.ect = ect;
 	}
-
 	public User() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
 	
-	@Id
+	@Id 
 	public String getPseudo() {
 		return pseudo;
 	}
-
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
 	}
-
 	public String getFn() {
 		return fn;
 	}
-
 	public void setFn(String fn) {
 		this.fn = fn;
 	}
-
 	public String getLn() {
 		return ln;
 	}
-
 	public void setLn(String ln) {
 		this.ln = ln;
 	}
-
 	public int getCodep() {
 		return codep;
 	}
-
 	public void setCodep(int codep) {
 		this.codep = codep;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public boolean isSell_rights() {
 		return sell_rights;
 	}
-
 	public void setSell_rights(boolean sell_rights) {
 		this.sell_rights = sell_rights;
 	}
-
+	public Auction getEct() {
+		return ect;
+	}
+	public void setEct(Auction ect) {
+		this.ect = ect;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "User [pseudo=" + pseudo + ", fn=" + fn + ", ln=" + ln + ", codep=" + codep + ", email=" + email
-				+ ", sell_rights=" + sell_rights + "]";
+				+ ", sell_rights=" + sell_rights + ", ect=" + ect + "]";
 	}
+	
+	
 	
 	
 	
